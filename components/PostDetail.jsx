c411ce517abbcf179925d78c54477031e117a0ba
Manas <rockingmanas78@gmail.com>
2021-12-17 22:31:12 +0530

    initial

---
+++ components/PostDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostDetail = ({ post }) => {
+    return (
+        <div className="">
+            <h1>PostDetail</h1>
+            
+        </div>
+    )
+}
+
+export default PostDetail
